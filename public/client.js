61841a63644b73aaacf732f446a4bd6c48ba0642
GermanDZ <germ@ndz.com.ar>
2012-06-16 20:51:59 +0000

    fixes

--- public/client.js
+++ public/client.js
@@ -34,34 +34,36 @@
     show: function(hashesStr) {
       var code, hashes, latLng, showHash, _i, _len,
         _this = this;
-      hashes = hashesStr.split(" ");
-      showHash = function(code) {
-        var latLng, latLngBounds, pos1, pos2, rectangle;
-        latLng = decodeGeoHash(code);
-        pos1 = new google.maps.LatLng(latLng.latitude[0], latLng.longitude[0]);
-        pos2 = new google.maps.LatLng(latLng.latitude[1], latLng.longitude[1]);
-        latLngBounds = new google.maps.LatLngBounds(pos1, pos2);
-        rectangle = new google.maps.Rectangle({
-          map: _this.map,
-          fillColor: '#FE904D',
-          fillOpacity: 0.5,
-          strokeColor: '#4D90FE',
-          strokeOpacity: 0.5
+      if (hashesStr) {
+        hashes = hashesStr.split(" ");
+        showHash = function(code) {
+          var latLng, latLngBounds, pos1, pos2, rectangle;
+          latLng = decodeGeoHash(code);
+          pos1 = new google.maps.LatLng(latLng.latitude[0], latLng.longitude[0]);
+          pos2 = new google.maps.LatLng(latLng.latitude[1], latLng.longitude[1]);
+          latLngBounds = new google.maps.LatLngBounds(pos1, pos2);
+          rectangle = new google.maps.Rectangle({
+            map: _this.map,
+            fillColor: '#FE904D',
+            fillOpacity: 0.5,
+            strokeColor: '#4D90FE',
+            strokeOpacity: 0.5
+          });
+          return rectangle.setBounds(latLngBounds);
+        };
+        for (_i = 0, _len = hashes.length; _i < _len; _i++) {
+          code = hashes[_i];
+          showHash(code);
+        }
+        latLng = decodeGeoHash(hashes[0]);
+        latLng = new google.maps.LatLng(latLng.latitude[2], latLng.longitude[2]);
+        return new google.maps.Marker({
+          position: latLng,
+          animation: google.maps.Animation.DROP,
+          icon: this.markerImage,
+          map: this.map
         });
-        return rectangle.setBounds(latLngBounds);
-      };
-      for (_i = 0, _len = hashes.length; _i < _len; _i++) {
-        code = hashes[_i];
-        showHash(code);
       }
-      latLng = decodeGeoHash(hashes[0]);
-      latLng = new google.maps.LatLng(latLng.latitude[2], latLng.longitude[2]);
-      return new google.maps.Marker({
-        position: latLng,
-        animation: google.maps.Animation.DROP,
-        icon: this.markerImage,
-        map: this.map
-      });
     },
     "import": function(id) {
       var _this = this;
--- views/coffeescript/client.coffee
+++ views/coffeescript/client.coffee
@@ -26,27 +26,28 @@ window.RiderState =
     @markerImage.anchor = new google.maps.Point(6, 5)
 
   show: (hashesStr)->
-    hashes = hashesStr.split(" ")
-    showHash = (code) =>
-      latLng = decodeGeoHash(code)
-      pos1 = new google.maps.LatLng(latLng.latitude[0], latLng.longitude[0])
-      pos2 = new google.maps.LatLng(latLng.latitude[1], latLng.longitude[1])
-      latLngBounds = new google.maps.LatLngBounds(pos1, pos2)
-      rectangle = new google.maps.Rectangle
-        map: @map
-        fillColor: '#FE904D'
-        fillOpacity: 0.5
-        strokeColor: '#4D90FE'
-        strokeOpacity: 0.5
-      rectangle.setBounds(latLngBounds)
-    showHash code for code in hashes
-    latLng = decodeGeoHash(hashes[0])
-    latLng = new google.maps.LatLng(latLng.latitude[2], latLng.longitude[2])
-    new google.maps.Marker
-       position: latLng
-       animation: google.maps.Animation.DROP
-       icon: @markerImage
-       map: @map
+    if hashesStr
+        hashes = hashesStr.split(" ")
+        showHash = (code) =>
+          latLng = decodeGeoHash(code)
+          pos1 = new google.maps.LatLng(latLng.latitude[0], latLng.longitude[0])
+          pos2 = new google.maps.LatLng(latLng.latitude[1], latLng.longitude[1])
+          latLngBounds = new google.maps.LatLngBounds(pos1, pos2)
+          rectangle = new google.maps.Rectangle
+            map: @map
+            fillColor: '#FE904D'
+            fillOpacity: 0.5
+            strokeColor: '#4D90FE'
+            strokeOpacity: 0.5
+          rectangle.setBounds(latLngBounds)
+        showHash code for code in hashes
+        latLng = decodeGeoHash(hashes[0])
+        latLng = new google.maps.LatLng(latLng.latitude[2], latLng.longitude[2])
+        new google.maps.Marker
+           position: latLng
+           animation: google.maps.Animation.DROP
+           icon: @markerImage
+           map: @map
 
   import: (id)->
     $.ajax
